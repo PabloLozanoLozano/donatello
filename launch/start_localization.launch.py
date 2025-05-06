320cf64e77004924e2d0d96646419cafa59472b9
PabloLozanoLozano <pablolozloz22@uma.es>
2025-05-06 18:39:01 +0200

    slam and localization launch files

---
+++ launch/start_localization.launch.py
@@ -0,0 +1,25 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument
+
+def generate_launch_description():
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'slam_params_file',
+            default_value='./src/donatello/config/localization_params_online_async.yaml',
+            description='Archivo de parámetros para SLAM Toolbox'
+        ),
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='false',
+            description='Usar tiempo simulado'
+        ),
+        Node(
+            package='slam_toolbox',
+            executable='async_slam_toolbox_node',
+            name='slam_toolbox',
+            output='screen',
+            parameters=[LaunchConfiguration('slam_params_file'), {'use_sim_time': LaunchConfiguration('use_sim_time')}]
+        )
+    ])
